6465914d993c195eef6f0966af32d610cb04cb7d
kikiisa <kikiisa89@gmail.com>
2023-02-13 12:44:33 +0700

    commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  setup() {
+  },
+}
+</script>
+<template>
+   <section class="py-2 hero">
+    <div class="row justify-content-center">
+      <div class="col-md-8 col-lg-6 col-12">
+        <h1 class="text-center web-title color-2">
+          <strong>Mono </strong>
+          <span class="color-1 thumb"> Chrome</span>
+        </h1>
+        <p class="text-center sub-title">
+          merupakan badan usaha yang bergerak dibidang pelayanan jasa dan pengembangan sistem operasi dan konsultan It baik dari platform mobile, Desktop Dan Web. Monocrhome sendiri di bangun oleh 10 mahasiswa yang berasal dari kejuruan Teknik Informatika dari Universitas berbeda yang berada di Gorontalo.
+          😅😅
+        </p>
+      </div>
+    </div>
+  </section>
+  <section>
+
+  </section>
+</template>
+
+<style scoped>
+  section {font-family: 'Quicksand', sans-serif};
+  .sub-title{
+    font-weight: 400;
+  }
+  .thumb {
+  background-color: #3d3d3d;
+  border-radius: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  color: white;
+}
+  
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import './style.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+import App from './App.vue'
+createApp(App).mount('#app')
+import "bootstrap/dist/js/bootstrap.js"
